542dbf521ac9d22825cc180c4ab6d70bcf2d5717
mayonaiys <49065703+mayonaiys@users.noreply.github.com>
2020-02-13 08:50:44 +0100

    test

--- main.cpp
+++ main.cpp
@@ -5,6 +5,5 @@ int main(int argc, char **argv)
         QApplication app (argc, argv);
         QPushButton button ("Hello world !");
         button.show();
-        int test;
     return app.exec();
 }
